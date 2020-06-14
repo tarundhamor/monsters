9d830b38beb9c157715452b5c2ebfb717342495c
tarundhamor <tarundhamor@gmail.com>
2020-06-14 12:50:20 +0530

    Updates

---
+++ precache-manifest.fb2b44131eea884019b6e7f47a41c734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65dc3074ee9b4a5ae31ec9ced17133ad",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5befece7ed6318335fd9",
+    "url": "/monsters/static/css/main.a5ccc569.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5befece7ed6318335fd9",
+    "url": "/monsters/static/js/main.7589fd29.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
